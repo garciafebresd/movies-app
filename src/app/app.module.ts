ab941e065f2ef3802acdda40c2b7e1f00416d31b
Tecnico2011 <garciafebresd@gmail.com>
2020-02-20 01:05:33 -0300

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MoviesService } from './services/movies.service';
+
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { SearchComponent } from './components/search/search.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    MovieComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    MoviesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientJsonpModule } from '@angular/common/http';
+import { map } from 'rxjs/operators'; // Map
+
+@Injectable()
+export class MoviesService {
+
+  private apikey: string = "USA TU API KEY AQUI!!!!";
+  private urlMoviedb: string = "https://api.themoviedb.org/3";
+
+  constructor(private jsonp: HttpClientJsonpModule) { }
+
+  getPopulares() {
+
+    let url = `${this.urlMoviedb}/discover/movie?sort_by=popularity.desc&api_key=${this.apikey}&language=es&callback=JSONP_CALLBACK`;
+
+    return this.jsonp.get(url)
+      .map(res => res.json());
+  }
+
+  buscarPelicula(texto: string) {
+
+    let url = `${this.urlMoviedb}/search/movie?query=${texto}&sort_by=popularity.desc&api_key=${this.apikey}&language=es&callback=JSONP_CALLBACK`;
+
+    return this.jsonp.get(url)
+      .map(res => res.json());
+  }
+
+}
